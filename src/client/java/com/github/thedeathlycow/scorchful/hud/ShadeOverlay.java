5f3d3b6f593643a89bc21efa4101faf7cae9db52
TheDeathlyCow <thedeathlycow@gmail.com>
2024-01-10 00:48:08 -1000

    sun hat shading

---
+++ src/client/java/com/github/thedeathlycow/scorchful/hud/ShadeOverlay.java
@@ -0,0 +1,37 @@
+package com.github.thedeathlycow.scorchful.hud;
+
+import com.github.thedeathlycow.scorchful.Scorchful;
+import com.github.thedeathlycow.scorchful.config.ClientConfig;
+import com.github.thedeathlycow.scorchful.registry.SItems;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.BiConsumer;
+
+public class ShadeOverlay {
+
+    public static final Identifier SHADE_OVERLAY = Scorchful.id("textures/misc/shade_overlay.png");
+
+    public static void renderShadeOverlay(
+            DrawContext context,
+            @Nullable ClientPlayerEntity player,
+            BiConsumer<DrawContext, Float> renderCallback
+    ) {
+
+
+        if ( player != null && player.getEquippedStack(EquipmentSlot.HEAD).isOf(SItems.SUN_HAT)) {
+            ClientConfig config = Scorchful.getConfig().clientConfig;
+            if (config.isSunHatShading()) {
+                renderCallback.accept(context, config.getSunHatShadeOpacity());
+            }
+        }
+    }
+
+    private ShadeOverlay() {
+
+    }
+
+}
---
+++ src/client/java/com/github/thedeathlycow/scorchful/mixin/client/InGameHudMixin.java
@@ -0,0 +1,41 @@
+package com.github.thedeathlycow.scorchful.mixin.client;
+
+import com.github.thedeathlycow.scorchful.hud.ShadeOverlay;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+
+    @Shadow @Final private MinecraftClient client;
+
+
+    @Shadow protected abstract void renderOverlay(DrawContext context, Identifier texture, float opacity);
+
+    @Inject(
+            method = "render",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/network/ClientPlayerEntity;getFrozenTicks()I",
+                    shift = At.Shift.BEFORE
+            )
+    )
+    private void renderOverlays(DrawContext context, float tickDelta, CallbackInfo ci) {
+        ShadeOverlay.renderShadeOverlay(
+                context,
+                client.player,
+                (ctx, opacity) -> {
+                    this.renderOverlay(ctx, ShadeOverlay.SHADE_OVERLAY, opacity);
+                }
+        );
+    }
+
+}
