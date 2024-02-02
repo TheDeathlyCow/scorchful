594ce65257ef96b0546a5f6e2e7d28cc5145bce9
TheDeathlyCow <thedeathlycow@gmail.com>
2024-02-01 23:07:44 -1000

    water drips

--- src/main/java/com/github/thedeathlycow/scorchful/block/CrimsonLilyBlock.java
+++ src/main/java/com/github/thedeathlycow/scorchful/block/CrimsonLilyBlock.java
@@ -2,19 +2,31 @@ package com.github.thedeathlycow.scorchful.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
+@SuppressWarnings("deprecation")
 public class CrimsonLilyBlock extends Block {
 
     public static final EnumProperty<WaterSaturationLevel> WATER_SATURATION_LEVEL = EnumProperty.of(
             "water_level", WaterSaturationLevel.class
     );
+
+    private static final VoxelShape SHAPE = Block.createCuboidShape(
+            2.0, 0.0, 2.0,
+            14.0, 3.0, 14.0
+    );
+
 
     public CrimsonLilyBlock(Settings settings) {
         super(settings);
@@ -36,7 +48,25 @@ public class CrimsonLilyBlock extends Block {
 
     @Override
     public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
-        super.randomDisplayTick(state, world, pos, random);
+
+        WaterSaturationLevel saturationLevel = state.get(WATER_SATURATION_LEVEL);
+        float splashChance = saturationLevel.getSplashChance();
+
+        for (int i = 0; i < saturationLevel.getParticlesPerDisplayTick(); i++) {
+            double x = pos.getX() + random.nextDouble();
+            double y = pos.getY() + (random.nextDouble() / 4.0);
+            double z = pos.getZ() + random.nextDouble();
+
+            ParticleEffect particle = random.nextFloat() < splashChance
+                    ? ParticleTypes.DRIPPING_DRIPSTONE_WATER
+                    : ParticleTypes.DRIPPING_WATER;
+
+            world.addParticle(
+                    particle,
+                    x, y, z,
+                    0, 0, 0
+            );
+        }
     }
 
     @Override
@@ -47,5 +77,10 @@ public class CrimsonLilyBlock extends Block {
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 }
--- src/main/java/com/github/thedeathlycow/scorchful/block/WaterSaturationLevel.java
+++ src/main/java/com/github/thedeathlycow/scorchful/block/WaterSaturationLevel.java
@@ -4,15 +4,29 @@ import net.minecraft.util.StringIdentifiable;
 
 public enum WaterSaturationLevel implements StringIdentifiable {
 
-    DRY("dry"),
-    MOIST("moist"),
-    WET("wet"),
-    SOAKED("soaked");
+    DRY("dry", 0, 0f),
+    MOIST("moist", 0, 0f),
+    WET("wet", 1, 0f),
+    SOAKED("soaked", 8, 0.25f);
 
     private final String name;
 
-    WaterSaturationLevel(String name) {
+    private final int particlesPerTick;
+
+    private final float splashChance;
+
+    WaterSaturationLevel(String name, int particlesPerTick, float splashChance) {
         this.name = name;
+        this.particlesPerTick = particlesPerTick;
+        this.splashChance = splashChance;
+    }
+
+    public float getSplashChance() {
+        return splashChance;
+    }
+
+    public int getParticlesPerDisplayTick() {
+        return particlesPerTick;
     }
 
     @Override
