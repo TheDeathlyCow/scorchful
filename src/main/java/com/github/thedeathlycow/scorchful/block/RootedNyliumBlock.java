4fb51f1385288807e6b4014a1985c1674b8c555c
TheDeathlyCow <thedeathlycow@gmail.com>
2024-02-05 02:23:53 -1000

    decay rooted nylium to rooted netherrack if cant stayalive

--- src/main/java/com/github/thedeathlycow/scorchful/block/RootedNyliumBlock.java
+++ src/main/java/com/github/thedeathlycow/scorchful/block/RootedNyliumBlock.java
@@ -1,14 +1,14 @@
 package com.github.thedeathlycow.scorchful.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.Fertilizable;
+import com.github.thedeathlycow.scorchful.registry.SBlocks;
+import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.chunk.light.ChunkLightProvider;
 
 import java.util.function.Supplier;
 
@@ -19,6 +19,13 @@ public class RootedNyliumBlock extends Block implements Fertilizable {
     public RootedNyliumBlock(Settings settings, Supplier<BlockState> rootsProvider) {
         super(settings);
         this.rootsProvider = rootsProvider;
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        if (!stayAlive(state, world, pos)) {
+            world.setBlockState(pos, SBlocks.ROOTED_NETHERRACK.getDefaultState());
+        }
     }
 
     @Override
@@ -34,5 +41,12 @@ public class RootedNyliumBlock extends Block implements Fertilizable {
     @Override
     public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
         world.setBlockState(pos.up(), this.rootsProvider.get());
+    }
+
+    private static boolean stayAlive(BlockState state, WorldView world, BlockPos pos) {
+        BlockPos blockPos = pos.up();
+        BlockState blockState = world.getBlockState(blockPos);
+        int i = ChunkLightProvider.getRealisticOpacity(world, state, pos, blockState, blockPos, Direction.UP, blockState.getOpacity(world, blockPos));
+        return i < world.getMaxLightLevel();
     }
 }
