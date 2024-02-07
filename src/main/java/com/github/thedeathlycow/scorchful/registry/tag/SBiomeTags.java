155266cc337de63a0e2723ec41f3d5c334ac2bf5
TheDeathlyCow <thedeathlycow@gmail.com>
2024-02-07 00:23:10 -1000

    rename rooted crimson lily feature to crimson lily patch

--- src/main/java/com/github/thedeathlycow/scorchful/registry/tag/SBiomeTags.java
+++ src/main/java/com/github/thedeathlycow/scorchful/registry/tag/SBiomeTags.java
@@ -14,8 +14,8 @@ public class SBiomeTags {
 
     public static final TagKey<Biome> HUMID_BIOMES = SBiomeTags.register("humid_biomes");
 
-    public static final TagKey<Biome> HAS_FEATURE_ROOTED_CRIMSON_LILY = SBiomeTags.register("has_feature/rooted_crimson_lily");
-    public static final TagKey<Biome> HAS_FEATURE_SPARSE_ROOTED_CRIMSON_LILY = SBiomeTags.register("has_feature/sparse_rooted_crimson_lily");
+    public static final TagKey<Biome> HAS_FEATURE_CRIMSON_LILY_PATCH = SBiomeTags.register("has_feature/crimson_lily_patch");
+    public static final TagKey<Biome> HAS_FEATURE_SPARSE_CRIMSON_LILY_PATCH = SBiomeTags.register("has_feature/sparse_crimson_lily_patch");
 
     public static TagKey<Biome> register(String id) {
         return TagKey.of(RegistryKeys.BIOME, new Identifier(Scorchful.MODID, id));
