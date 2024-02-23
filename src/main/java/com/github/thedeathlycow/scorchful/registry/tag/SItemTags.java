815a8fed64f94826000ddd6e8b956e903dbbecbe
TheDeathlyCow <thedeathlycow@gmail.com>
2024-02-23 00:49:54 -1000

    is sun protecting hat item tag

--- src/main/java/com/github/thedeathlycow/scorchful/registry/tag/SItemTags.java
+++ src/main/java/com/github/thedeathlycow/scorchful/registry/tag/SItemTags.java
@@ -19,6 +19,8 @@ public class SItemTags {
 
     public static final TagKey<Item> HEAT_VERY_HARMFUL = of("heat_resistance/very_harmful");
 
+    public static final TagKey<Item> IS_SUN_PROTECTING_HAT = of("is_sun_protecting_hat");
+
     private static TagKey<Item> of(String path) {
         return TagKey.of(RegistryKeys.ITEM, Scorchful.id(path));
     }
