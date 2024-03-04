624f35097ae93af41f9eddca95e1573d634b549e
TheDeathlyCow <thedeathlycow@gmail.com>
2024-03-03 19:18:18 -1000

    apply slowness when outside in sandstorm
    closes #28

--- src/main/java/com/github/thedeathlycow/scorchful/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/thedeathlycow/scorchful/mixin/LivingEntityMixin.java
@@ -1,15 +1,29 @@
 package com.github.thedeathlycow.scorchful.mixin;
 
 import com.github.thedeathlycow.scorchful.event.ScorchfulLivingEntityEvents;
+import com.github.thedeathlycow.scorchful.server.SandstormSlowing;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin extends Entity {
+
+    @Unique
+    private boolean scorchful_wasInSandstorm = false;
+
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
     @Inject(
             method = "damage",
@@ -19,6 +33,20 @@ public class LivingEntityMixin {
         if (Boolean.TRUE.equals(cir.getReturnValue())) {
             ScorchfulLivingEntityEvents.ON_DAMAGED.invoker().onDamaged((LivingEntity) (Object) this, source, amount);
         }
+    }
+
+    @Inject(
+            method = "tickMovement",
+            at = @At("TAIL")
+    )
+    private void afterTickMovement(CallbackInfo ci) {
+        Profiler profiler = this.getWorld().getProfiler();
+        profiler.push("scorchful_sandstorm_slow");
+        scorchful_wasInSandstorm = SandstormSlowing.tickSandstormSlow(
+                (LivingEntity) (Object) this,
+                scorchful_wasInSandstorm
+        );
+        profiler.pop();
     }
 
 }
---
+++ src/main/java/com/github/thedeathlycow/scorchful/server/SandstormSlowing.java
@@ -0,0 +1,75 @@
+package com.github.thedeathlycow.scorchful.server;
+
+import com.github.thedeathlycow.scorchful.Scorchful;
+import com.github.thedeathlycow.scorchful.config.WeatherConfig;
+import com.github.thedeathlycow.scorchful.registry.tag.SEntityTypeTags;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.UUID;
+
+public class SandstormSlowing {
+
+    private static final UUID ATTRIBUTE_ID = UUID.fromString("253995f0-1ab9-4a3c-a440-acf6fae80ee5");
+
+    public static boolean tickSandstormSlow(LivingEntity entity, boolean wasInSandstorm) {
+
+        if (entity.getType().isIn(SEntityTypeTags.DOES_NOT_SLOW_IN_SANDSTORM)) {
+            return false;
+        }
+
+        World world = entity.getWorld();
+        BlockPos pos = entity.getBlockPos();
+
+        if (world.isClient) {
+            return false;
+        }
+
+        if (Sandstorms.getCurrentSandStorm(world, pos) == Sandstorms.SandstormType.NONE) {
+            if (wasInSandstorm) {
+                removeSlow(entity);
+            }
+            return false;
+        }
+
+        if (!wasInSandstorm) {
+            addSlow(entity);
+        }
+
+        return true;
+    }
+
+    private static void removeSlow(LivingEntity entity) {
+        EntityAttributeInstance instance = entity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        if (instance != null && instance.getModifier(ATTRIBUTE_ID) != null) {
+            instance.removeModifier(ATTRIBUTE_ID);
+        }
+    }
+
+    private static void addSlow(LivingEntity entity) {
+        EntityAttributeInstance instance = entity.getAttributeInstance(EntityAttributes.GENERIC_MOVEMENT_SPEED);
+        if (instance != null) {
+
+            WeatherConfig config = Scorchful.getConfig().weatherConfig;
+            double value = config.getSandstormSlownessAmountPercent();
+
+            instance.addTemporaryModifier(
+                    new EntityAttributeModifier(
+                            ATTRIBUTE_ID,
+                            "Sandstorm slow",
+                            value,
+                            EntityAttributeModifier.Operation.MULTIPLY_TOTAL
+                    )
+            );
+        }
+    }
+
+    private SandstormSlowing() {
+
+    }
+
+}
