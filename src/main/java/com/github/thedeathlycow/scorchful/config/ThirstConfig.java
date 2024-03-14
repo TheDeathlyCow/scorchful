cbd1136d5e18adc9efc88dbdc61bbc68c6a27154
TheDeathlyCow <thedeathlycow@gmail.com>
2024-03-14 01:47:13 -1000

    reduce temp from wetness by half

--- src/main/java/com/github/thedeathlycow/scorchful/config/ThirstConfig.java
+++ src/main/java/com/github/thedeathlycow/scorchful/config/ThirstConfig.java
@@ -7,7 +7,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = Scorchful.MODID + ".thirst_config")
 public class ThirstConfig implements ConfigData {
 
-    int temperatureFromWetness = -12;
+    int temperatureFromWetness = -6;
 
     int waterFromRefreshingFood = 60;
 
