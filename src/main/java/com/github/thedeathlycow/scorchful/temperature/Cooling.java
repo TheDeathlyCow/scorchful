69a441fb6ecd0fcca3bacc0be94a61d58d3fa1b3
TheDeathlyCow <thedeathlycow@gmail.com>
2024-03-18 01:16:50 -1000

    tick cooling for all living entities
    Fixes #34

---
+++ src/main/java/com/github/thedeathlycow/scorchful/temperature/Cooling.java
@@ -0,0 +1,33 @@
+package com.github.thedeathlycow.scorchful.temperature;
+
+import com.github.thedeathlycow.scorchful.Scorchful;
+import com.github.thedeathlycow.scorchful.config.ScorchfulConfig;
+import com.github.thedeathlycow.scorchful.config.ThirstConfig;
+import com.github.thedeathlycow.scorchful.registry.tag.SBiomeTags;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+
+public class Cooling {
+
+    public static void tick(LivingEntity entity) {
+        if (entity.thermoo$getTemperature() > 0 && entity.thermoo$isWet()) {
+            ThirstConfig config = Scorchful.getConfig().thirstConfig;
+
+            int temperatureChange = MathHelper.floor(
+                    config.getTemperatureFromWetness() * entity.thermoo$getSoakedScale()
+            );
+            World world = entity.getWorld();
+            if (world.getBiome(entity.getBlockPos()).isIn(SBiomeTags.HUMID_BIOMES)) {
+                temperatureChange = MathHelper.floor(temperatureChange * config.getHumidBiomeSweatEfficiency());
+            }
+
+            entity.thermoo$addTemperature(temperatureChange);
+        }
+    }
+
+    private Cooling() {
+
+    }
+
+}
