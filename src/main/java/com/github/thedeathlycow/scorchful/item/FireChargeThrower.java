52a03ce4536cd4e2a0a882868332de823724758e
TheDeathlyCow <thedeathlycow@gmail.com>
2024-03-28 00:02:27 -1000

    get fire balls to move properly and nicely

--- src/main/java/com/github/thedeathlycow/scorchful/item/FireChargeThrower.java
+++ src/main/java/com/github/thedeathlycow/scorchful/item/FireChargeThrower.java
@@ -1,19 +1,24 @@
 package com.github.thedeathlycow.scorchful.item;
 
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
-import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.AbstractFireballEntity;
+import net.minecraft.entity.projectile.SmallFireballEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
 
 public class FireChargeThrower implements UseItemCallback {
 
+    private static final int FIRE_CHARGE_COOL_DOWN = 20;
+
     @Override
     public TypedActionResult<ItemStack> interact(PlayerEntity player, World world, Hand hand) {
-        if (player.isSpectator() || world.isClient) {
+        if (player.isSpectator()) {
             return TypedActionResult.pass(ItemStack.EMPTY);
         }
 
@@ -21,25 +26,29 @@ public class FireChargeThrower implements UseItemCallback {
         if (!stack.isOf(Items.FIRE_CHARGE)) {
             return TypedActionResult.pass(ItemStack.EMPTY);
         }
-
-        // spawn fire charge entity
-        var fireball = EntityType.SMALL_FIREBALL.create(world);
-
-        if (fireball == null) {
+        if (player.getItemCooldownManager().isCoolingDown(stack.getItem())) {
             return TypedActionResult.fail(stack);
         }
 
-        fireball.setPosition(player.getX(), player.getBodyY(0.5) + 0.5, player.getZ());
-
-        if (!world.spawnEntity(fireball)) {
-            return TypedActionResult.fail(stack);
+        // spawn fire charge entity
+        if (!world.isClient) {
+            Vec3d rotation = player.getRotationVector();
+            AbstractFireballEntity fireball = new SmallFireballEntity(
+                    world,
+                    player,
+                    rotation.x, rotation.y, rotation.z
+            );
+            fireball.setPosition(fireball.getX(), player.getBodyY(0.5) + 0.5, fireball.getZ());
+            fireball.setItem(stack);
+            world.spawnEntity(fireball);
+            world.syncWorldEvent(null, WorldEvents.BLAZE_SHOOTS, player.getBlockPos(), 0);
         }
 
         // decrement stack
         if (!player.isCreative()) {
             stack.decrement(1);
         }
-        player.getItemCooldownManager().set(stack.getItem(), 20);
+        player.getItemCooldownManager().set(stack.getItem(), FIRE_CHARGE_COOL_DOWN);
 
         return TypedActionResult.success(stack);
     }
