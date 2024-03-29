f342985ce62075de5c505a63c2360fba8ae9d277
TheDeathlyCow <thedeathlycow@gmail.com>
2024-03-29 01:43:36 -1000

    translate maxThirstAddedByRehydration integration config

--- src/main/java/com/github/thedeathlycow/scorchful/config/CombatConfig.java
+++ src/main/java/com/github/thedeathlycow/scorchful/config/CombatConfig.java
@@ -4,10 +4,12 @@ import com.github.thedeathlycow.scorchful.Scorchful;
 import com.github.thedeathlycow.scorchful.item.FireChargeThrower;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = Scorchful.MODID + ".combat_config")
 public class CombatConfig implements ConfigData {
 
+    @ConfigEntry.Gui.EnumHandler
     FireChargeThrower.FireballFactory fireBallThrownType = FireChargeThrower.FireballFactory.SMALL;
 
     public FireChargeThrower.FireballFactory getFireBallThrownType() {
