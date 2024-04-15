78d820b42b90eef9195fc7a32c42654bad788757
TheDeathlyCow <thedeathlycow@gmail.com>
2024-04-14 15:50:59 -1000

    extract mixin logic

---
+++ src/client/java/com/github/thedeathlycow/scorchful/client/HeatStrokeEffects.java
@@ -0,0 +1,48 @@
+package com.github.thedeathlycow.scorchful.client;
+
+import com.github.thedeathlycow.scorchful.Scorchful;
+import com.github.thedeathlycow.scorchful.mixin.client.GameRendererAccessor;
+import com.github.thedeathlycow.scorchful.registry.SStatusEffects;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.packet.s2c.play.EntityStatusEffectS2CPacket;
+import net.minecraft.network.packet.s2c.play.RemoveEntityStatusEffectS2CPacket;
+import net.minecraft.util.Identifier;
+
+@Environment(EnvType.CLIENT)
+public class HeatStrokeEffects {
+
+    public static final float HALF_MINUTE_TICKS = 30f;
+
+    public static final String TIME_UNIFORM_NAME = "ScorchfulHalfMinuteTime";
+
+    private static final Identifier HEAT_STROKE_SHADER_ID = Scorchful.id("shaders/post/heat_stroke.json");
+
+    public static void onEffectAdded(EntityStatusEffectS2CPacket packet, ClientWorld world, MinecraftClient client) {
+        if (packet.getEffectId() == SStatusEffects.HEAT_STROKE) {
+            Entity entity = world.getEntityById(packet.getEntityId());
+            if (entity instanceof ClientPlayerEntity player && player.isMainPlayer()) {
+                ((GameRendererAccessor) client.gameRenderer)
+                        .invokeLoadPostProcessor(HEAT_STROKE_SHADER_ID);
+            }
+        }
+    }
+
+    public static void onEffectRemoved(RemoveEntityStatusEffectS2CPacket packet, ClientWorld world, MinecraftClient client) {
+        if (packet.getEffectType() == SStatusEffects.HEAT_STROKE) {
+            Entity entity = packet.getEntity(world);
+            if (entity instanceof ClientPlayerEntity player && player.isMainPlayer()) {
+                client.gameRenderer.disablePostProcessor();
+            }
+        }
+    }
+
+    private HeatStrokeEffects() {
+
+    }
+
+}
--- src/client/java/com/github/thedeathlycow/scorchful/mixin/client/ClientPlayNetworkHandlerMixin.java
+++ src/client/java/com/github/thedeathlycow/scorchful/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -1,12 +1,9 @@
 package com.github.thedeathlycow.scorchful.mixin.client;
 
-import com.github.thedeathlycow.scorchful.Scorchful;
-import com.github.thedeathlycow.scorchful.registry.SStatusEffects;
+import com.github.thedeathlycow.scorchful.client.HeatStrokeEffects;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.Entity;
 import net.minecraft.network.packet.s2c.play.EntityStatusEffectS2CPacket;
 import net.minecraft.network.packet.s2c.play.RemoveEntityStatusEffectS2CPacket;
 import org.spongepowered.asm.mixin.Final;
@@ -19,23 +16,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
 
-    @Shadow private ClientWorld world;
+    @Shadow
+    private ClientWorld world;
 
-    @Shadow @Final private MinecraftClient client;
+    @Shadow
+    @Final
+    private MinecraftClient client;
 
     @Inject(
             method = "onEntityStatusEffect",
             at = @At("TAIL")
     )
     private void onEffectAdded(EntityStatusEffectS2CPacket packet, CallbackInfo ci) {
-        if (packet.getEffectId() == SStatusEffects.HEAT_STROKE) {
-            Entity entity = this.world.getEntityById(packet.getEntityId());
-            if (entity instanceof ClientPlayerEntity player && player.isMainPlayer()) {
-                ((GameRendererAccessor) this.client.gameRenderer).invokeLoadPostProcessor(
-                        Scorchful.id("shaders/post/heat_stroke.json")
-                );
-            }
-        }
+        HeatStrokeEffects.onEffectAdded(packet, this.world, this.client);
     }
 
     @Inject(
@@ -43,12 +36,7 @@ public class ClientPlayNetworkHandlerMixin {
             at = @At("TAIL")
     )
     private void onEffectRemoved(RemoveEntityStatusEffectS2CPacket packet, CallbackInfo ci) {
-        if (packet.getEffectType() == SStatusEffects.HEAT_STROKE) {
-            Entity entity = packet.getEntity(this.world);
-            if (entity instanceof ClientPlayerEntity player && player.isMainPlayer()) {
-                this.client.gameRenderer.disablePostProcessor();
-            }
-        }
+        HeatStrokeEffects.onEffectRemoved(packet, this.world, this.client);
     }
 
 }
