6c92be458139bdf765d1657713538f9b94f336f5
TheDeathlyCow <thedeathlycow@gmail.com>
2024-08-07 01:34:37 -1000

    fear effect and goal

---
+++ src/main/java/com/github/thedeathlycow/scorchful/entity/effect/FearStatusEffect.java
@@ -0,0 +1,10 @@
+package com.github.thedeathlycow.scorchful.entity.effect;
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+
+public class FearStatusEffect extends StatusEffect {
+    public FearStatusEffect(StatusEffectCategory category, int color) {
+        super(category, color);
+    }
+}
---
+++ src/main/java/com/github/thedeathlycow/scorchful/mixin/MobEntityMixin.java
@@ -0,0 +1,40 @@
+package com.github.thedeathlycow.scorchful.mixin;
+
+import com.github.thedeathlycow.scorchful.entity.ai.FearTargetGoal;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MobEntity.class)
+public abstract class MobEntityMixin extends LivingEntity {
+
+    @Shadow
+    @Final
+    protected GoalSelector goalSelector;
+
+    protected MobEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(
+            method = "<init>",
+            at = @At("TAIL")
+    )
+    private void initFearTargetGoal(CallbackInfo ci) {
+        if (this.getWorld() != null && !this.getWorld().isClient) {
+            MobEntity instance = (MobEntity) (Object) this;
+            if (instance instanceof PathAwareEntity pathAware) {
+                this.goalSelector.add(1, new FearTargetGoal(pathAware));
+            }
+        }
+    }
+}
