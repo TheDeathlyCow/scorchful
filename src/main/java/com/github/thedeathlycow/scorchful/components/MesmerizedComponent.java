0130cdf712bdc9f3eb78738d05a868ea5092dccd
TheDeathlyCow <thedeathlycow@gmail.com>
2024-09-12 19:33:02 -1000

    force player movement when mesmerized

--- src/main/java/com/github/thedeathlycow/scorchful/components/MesmerizedComponent.java
+++ src/main/java/com/github/thedeathlycow/scorchful/components/MesmerizedComponent.java
@@ -2,16 +2,21 @@ package com.github.thedeathlycow.scorchful.components;
 
 import com.github.thedeathlycow.scorchful.entity.effect.MesmerizedStatusEffect;
 import com.github.thedeathlycow.scorchful.registry.SStatusEffects;
-import net.minecraft.entity.Entity;
+import net.minecraft.command.argument.EntityAnchorArgumentType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
+import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.math.Vec3d;
 import org.ladysnake.cca.api.v3.component.Component;
 import org.ladysnake.cca.api.v3.component.tick.ServerTickingComponent;
 
 import java.util.List;
 
 public class MesmerizedComponent implements Component, ServerTickingComponent {
+
+    private static final double MOVEMENT_SPEED = 0.07;
 
     private final LivingEntity provider;
 
@@ -43,22 +48,72 @@ public class MesmerizedComponent implements Component, ServerTickingComponent {
 
     @Override
     public void serverTick() {
-        if (this.mesmerizedTarget != null) {
-            boolean removeTarget = !this.provider.hasStatusEffect(SStatusEffects.MESMERIZED)
-                    || !MesmerizedStatusEffect.IS_VALID_TARGET.test(this.mesmerizedTarget);
-            if (removeTarget) {
-                this.mesmerizedTarget = null;
-            }
-        } else {
-            List<LivingEntity> nearby = this.provider.getWorld().getEntitiesByClass(
-                    LivingEntity.class,
-                    this.provider.getBoundingBox().expand(16),
-                    MesmerizedStatusEffect.IS_VALID_TARGET
-                            .and(entity -> entity != this.provider)
-            );
-            if (!nearby.isEmpty()) {
-                this.setMesmerizedTarget(nearby.getFirst());
-            }
+        if (!this.provider.hasStatusEffect(SStatusEffects.MESMERIZED)) {
+            return;
         }
+
+        if (this.updateTarget()) {
+            this.moveTowardsTarget();
+        }
+    }
+
+    /**
+     * @return Returns true if the provider has a mesmerized target after this call
+     */
+    private boolean updateTarget() {
+        if (this.hasMesmerizedTarget()) {
+            return this.checkKeepMesmerizedTarget();
+        } else {
+            return this.tryAcquireNewMesmerizedTarget();
+        }
+    }
+
+    private void moveTowardsTarget() {
+        if (this.canForceProviderMovement()) {
+            Vec3d targetPos = this.mesmerizedTarget.getPos();
+            Vec3d providerPos = this.provider.getPos();
+
+            Vec3d moveDirection = targetPos.subtract(providerPos)
+                    .normalize()
+                    .multiply(MOVEMENT_SPEED);
+
+            this.provider.addVelocity(moveDirection);
+            this.provider.velocityModified = true;
+
+            this.provider.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, targetPos);
+        }
+    }
+
+    /**
+     * @return Returns {@code true} if the current target can be kept
+     */
+    private boolean checkKeepMesmerizedTarget() {
+        if (MesmerizedStatusEffect.IS_VALID_TARGET.test(this.mesmerizedTarget)) {
+            return true;
+        } else {
+            this.mesmerizedTarget = null;
+            return false;
+        }
+    }
+
+    /**
+     * @return Returns {@code true} if a new target is found
+     */
+    private boolean tryAcquireNewMesmerizedTarget() {
+        List<LivingEntity> nearby = this.provider.getWorld().getEntitiesByClass(
+                LivingEntity.class,
+                this.provider.getBoundingBox().expand(16),
+                MesmerizedStatusEffect.IS_VALID_TARGET
+                        .and(entity -> entity != this.provider)
+        );
+        if (!nearby.isEmpty()) {
+            this.setMesmerizedTarget(nearby.getFirst());
+            return true;
+        }
+        return false;
+    }
+
+    private boolean canForceProviderMovement() {
+        return !(this.provider.isSpectator() || this.provider instanceof PathAwareEntity);
     }
 }
