2213f6f436b6f0e6ca254250c038ed9fb7afed71
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-04 20:43:45 -1000

    switch to item tags

--- src/main/java/com/github/thedeathlycow/scorchful/item/HeatResistanceHelper.java
+++ src/main/java/com/github/thedeathlycow/scorchful/item/HeatResistanceHelper.java
@@ -8,30 +8,38 @@ import com.github.thedeathlycow.thermoo.api.ThermooAttributes;
 import com.github.thedeathlycow.thermoo.api.armor.material.ArmorMaterialTags;
 import com.github.thedeathlycow.thermoo.api.item.ModifyItemAttributeModifiersCallback;
 import net.minecraft.component.type.AttributeModifierSlot;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
 
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.function.ToDoubleFunction;
 
 public class HeatResistanceHelper {
+    private static final Map<EquipmentSlot, Identifier> SLOT_TO_MODIFIER_ID = new EnumMap<>(EquipmentSlot.class);
+
     public static void initialize() {
         ModifyItemAttributeModifiersCallback.EVENT.register((stack, builder) -> {
             if (stack.getItem() instanceof ArmorItem armorItem) {
                 CombatConfig config = Scorchful.getConfig().combatConfig;
                 HeatResistanceLevel level = HeatResistanceLevel.forStack(stack);
+                EquipmentSlot slot = armorItem.getSlotType();
                 builder.add(
                         ThermooAttributes.HEAT_RESISTANCE,
                         new EntityAttributeModifier(
-                                Scorchful.id("base_heat_resistance"),
+                                SLOT_TO_MODIFIER_ID.computeIfAbsent(slot, s -> Scorchful.id("base_heat_resistance/" + s.asString())),
                                 level.getHeatResistance(config),
                                 EntityAttributeModifier.Operation.ADD_VALUE
                         ),
-                        AttributeModifierSlot.forEquipmentSlot(armorItem.getSlotType())
+                        AttributeModifierSlot.forEquipmentSlot(slot)
                 );
             }
         });
