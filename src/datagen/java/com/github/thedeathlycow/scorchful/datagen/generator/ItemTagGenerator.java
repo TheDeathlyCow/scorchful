1f473f42288987c710df496bc3060356edaff90e
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-13 16:01:13 -1000

    remove unused item heat resistance tags

--- src/datagen/java/com/github/thedeathlycow/scorchful/datagen/generator/ItemTagGenerator.java
+++ src/datagen/java/com/github/thedeathlycow/scorchful/datagen/generator/ItemTagGenerator.java
@@ -1,11 +1,8 @@
 package com.github.thedeathlycow.scorchful.datagen.generator;
 
-import com.github.thedeathlycow.scorchful.registry.tag.SItemTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
-import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.registry.tag.ItemTags;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.CompletableFuture;
@@ -17,24 +14,5 @@ public class ItemTagGenerator extends FabricTagProvider.ItemTagProvider {
 
     @Override
     protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-        getOrCreateTagBuilder(SItemTags.IS_VERY_PROTECTIVE_HEAT_RESISTANCE)
-                .addOptionalTag(SItemTags.TURTLE_ARMOR);
-
-        getOrCreateTagBuilder(SItemTags.IS_PROTECTIVE_HEAT_RESISTANCE)
-                .add(Items.NETHERITE_HELMET)
-                .add(Items.NETHERITE_CHESTPLATE)
-                .add(Items.NETHERITE_LEGGINGS)
-                .add(Items.NETHERITE_BOOTS)
-                .add(Items.WOLF_ARMOR);
-
-        getOrCreateTagBuilder(SItemTags.IS_NEUTRAL_HEAT_RESISTANCE)
-                .add(Items.GOLDEN_HELMET)
-                .add(Items.GOLDEN_CHESTPLATE)
-                .add(Items.GOLDEN_LEGGINGS)
-                .add(Items.GOLDEN_BOOTS)
-                .add(Items.CHAINMAIL_HELMET)
-                .add(Items.CHAINMAIL_CHESTPLATE)
-                .add(Items.CHAINMAIL_LEGGINGS)
-                .add(Items.CHAINMAIL_BOOTS);
     }
 }
