25e501e04dd6b34fb251d565427387831aff2b82
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-16 17:32:49 -1000

    remove unneeded on damaged event

--- src/main/java/com/github/thedeathlycow/scorchful/mixin/LivingEntityMixin.java
+++ src/main/java/com/github/thedeathlycow/scorchful/mixin/LivingEntityMixin.java
@@ -1,13 +1,11 @@
 package com.github.thedeathlycow.scorchful.mixin;
 
 import com.github.thedeathlycow.scorchful.entity.effect.FearStatusEffect;
-import com.github.thedeathlycow.scorchful.event.ScorchfulLivingEntityEvents;
 import com.github.thedeathlycow.scorchful.server.SandstormSlowing;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.util.profiler.Profiler;
 import net.minecraft.util.profiler.Profilers;
@@ -21,22 +19,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin extends Entity {
-
     @Unique
     private boolean scorchful_wasInSandstorm = false;
 
     public LivingEntityMixin(EntityType<?> type, World world) {
         super(type, world);
-    }
-
-    @Inject(
-            method = "damage",
-            at = @At("TAIL")
-    )
-    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if (Boolean.TRUE.equals(cir.getReturnValue())) {
-            ScorchfulLivingEntityEvents.ON_DAMAGED.invoker().onDamaged((LivingEntity) (Object) this, source, amount);
-        }
     }
 
     @Inject(
