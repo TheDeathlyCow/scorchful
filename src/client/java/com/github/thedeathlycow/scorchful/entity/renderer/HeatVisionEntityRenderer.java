1be510f8eb928250f85306ba7006c5b4f9604792
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-18 00:00:11 -1000

    render a stone block

--- src/client/java/com/github/thedeathlycow/scorchful/entity/renderer/HeatVisionEntityRenderer.java
+++ src/client/java/com/github/thedeathlycow/scorchful/entity/renderer/HeatVisionEntityRenderer.java
@@ -4,8 +4,12 @@ import com.github.thedeathlycow.scorchful.entity.HeatVisionEntity;
 import com.github.thedeathlycow.scorchful.entity.renderer.state.HeatVisionEntityRenderState;
 import com.github.thedeathlycow.scorchful.mixin.client.accessor.EntityRenderDispatcherAccessor;
 import com.github.thedeathlycow.scorchful.temperature.heatvision.v2.HeatVisionType;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.BlockRenderManager;
+import net.minecraft.client.render.entity.DisplayEntityRenderer;
 import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.state.EntityRenderState;
@@ -16,8 +20,11 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.HuskEntity;
 
 public class HeatVisionEntityRenderer extends EntityRenderer<HeatVisionEntity, HeatVisionEntityRenderState> {
+    private final BlockRenderManager blockRenderManager;
+
     public HeatVisionEntityRenderer(EntityRendererFactory.Context context) {
         super(context);
+        this.blockRenderManager = context.getBlockRenderManager();
     }
 
     @Override
@@ -37,19 +44,29 @@ public class HeatVisionEntityRenderer extends EntityRenderer<HeatVisionEntity, H
         super.render(state, matrices, vertexConsumers, light);
 
         switch (state.renderType) {
-            case HeatVisionType ENTITY when state.entityState != null -> {
-                EntityRenderDispatcherAccessor accessor = (EntityRenderDispatcherAccessor) MinecraftClient.getInstance().getEntityRenderDispatcher();
-                var renderer = (EntityRenderer<Entity, EntityRenderState>) accessor.renderers().get(state.entityState.type());
+            case ENTITY -> {
+                if (state.entityState != null) {
+                    EntityRenderDispatcherAccessor accessor = (EntityRenderDispatcherAccessor) MinecraftClient.getInstance().getEntityRenderDispatcher();
+                    var renderer = (EntityRenderer<Entity, EntityRenderState>) accessor.renderers().get(state.entityState.type());
 
-                EntityRenderState s = renderer.createRenderState();
-                s.onFire = state.entityState.onFire();
+                    EntityRenderState s = renderer.createRenderState();
+                    s.onFire = state.entityState.onFire();
 
-                renderer.render(s, matrices, vertexConsumers, light);
+                    renderer.render(s, matrices, vertexConsumers, light);
+                }
             }
             case BLOCK -> {
-
+                this.blockRenderManager.renderBlockAsEntity(
+                        Blocks.STONE.getDefaultState(),
+                        matrices,
+                        vertexConsumers,
+                        light,
+                        OverlayTexture.DEFAULT_UV
+                );
             }
-            default -> {}
+            default -> {
+                // render nothing when empty
+            }
         }
     }
 }
