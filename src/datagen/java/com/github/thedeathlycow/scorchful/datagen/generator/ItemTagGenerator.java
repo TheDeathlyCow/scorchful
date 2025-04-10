45f1d0c694a1291f31f5f401e2047c6b84e6af60
TheDeathlyCow <thedeathlycow@gmail.com>
2025-04-09 22:36:29 -1000

    move c tags to datagen

--- src/datagen/java/com/github/thedeathlycow/scorchful/datagen/generator/ItemTagGenerator.java
+++ src/datagen/java/com/github/thedeathlycow/scorchful/datagen/generator/ItemTagGenerator.java
@@ -5,6 +5,7 @@ import com.github.thedeathlycow.scorchful.registry.SItems;
 import com.github.thedeathlycow.scorchful.registry.tag.SItemTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
--- src/main/java/com/github/thedeathlycow/scorchful/registry/tag/SItemTags.java
+++ src/main/java/com/github/thedeathlycow/scorchful/registry/tag/SItemTags.java
@@ -4,6 +4,7 @@ import com.github.thedeathlycow.scorchful.Scorchful;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 public class SItemTags {
 
@@ -28,9 +29,12 @@ public class SItemTags {
 
     public static final TagKey<Item> COMMON_CACTUS_JUICE = ofCommon("drinks/cactus_juice");
 
-
     private static TagKey<Item> of(String path) {
         return TagKey.of(RegistryKeys.ITEM, Scorchful.id(path));
+    }
+
+    private static TagKey<Item> ofCommon(String path) {
+        return TagKey.of(RegistryKeys.ITEM, Identifier.of("c", path));
     }
 
     private SItemTags() {
