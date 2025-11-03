314202098097b3b60df898a3fb079e313a9d9829
TheDeathlyCow <thedeathlycow@gmail.com>
2025-11-02 19:00:03 -1000

    datagen

--- src/main/java/com/github/thedeathlycow/scorchful/config/section/SchemaConfig.java
+++ src/main/java/com/github/thedeathlycow/scorchful/config/section/SchemaConfig.java
@@ -20,7 +20,7 @@ public class SchemaConfig {
             )
             .build();
 
-    public static final int CONFIG_VERSION = 3;
+    public static final int CONFIG_VERSION = 4;
 
     @SerialEntry(comment = "Config schema version, do not touch! Changing this value may result in unexpected behaviour.")
     int schemaVersion = CONFIG_VERSION;
