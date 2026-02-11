a6cf8ff2f4c2a006a59e3a4fa1a41260fe8a2809
TheDeathlyCow <thedeathlycow@gmail.com>
2026-02-10 22:14:11 -1000

    use a more resilient method for registering attributes to player
    fixes #128

--- src/main/java/com/github/thedeathlycow/scorchful/mixin/PlayerMixin.java
+++ src/main/java/com/github/thedeathlycow/scorchful/mixin/PlayerMixin.java
@@ -3,13 +3,14 @@ package com.github.thedeathlycow.scorchful.mixin;
 import com.github.thedeathlycow.scorchful.item.TurtleArmorEffects;
 import com.github.thedeathlycow.scorchful.registry.SEntityAttributes;
 import com.github.thedeathlycow.scorchful.temperature.heatvision.VisionSpawner;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Player.class)
 public class PlayerMixin {
@@ -32,13 +33,13 @@ public class PlayerMixin {
         TurtleArmorEffects.update(player);
     }
 
-    @Inject(
-            method = "createAttributes",
-            at = @At("TAIL")
+    @WrapMethod(
+            method = "createAttributes"
     )
-    private static void appendAttributes(CallbackInfoReturnable<AttributeSupplier.Builder> cir) {
-        AttributeSupplier.Builder builder = cir.getReturnValue();
+    private static AttributeSupplier.Builder registerAttributes(Operation<AttributeSupplier.Builder> original) {
+        AttributeSupplier.Builder builder = original.call();
         builder.add(SEntityAttributes.REHYDRATION_EFFICIENCY);
         builder.add(SEntityAttributes.LUNG_CAPACITY);
+        return builder;
     }
 }
