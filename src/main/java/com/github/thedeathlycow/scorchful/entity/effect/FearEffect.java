832fb768d07508e6e977e7f5ae4728b4f1e5c6f8
TheDeathlyCow <thedeathlycow@gmail.com>
2026-04-08 00:56:21 -1000

    make armour bar red with fear effect

--- src/main/java/com/github/thedeathlycow/scorchful/entity/effect/FearEffect.java
+++ src/main/java/com/github/thedeathlycow/scorchful/entity/effect/FearEffect.java
@@ -4,6 +4,7 @@ import com.github.thedeathlycow.scorchful.config.ScorchfulConfig;
 import com.github.thedeathlycow.scorchful.registry.SMobEffects;
 import com.github.thedeathlycow.scorchful.registry.SParticleTypes;
 import com.github.thedeathlycow.scorchful.registry.tag.SEntityTypeTags;
+import net.minecraft.util.ARGB;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -12,6 +13,8 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.PathfinderMob;
 
 public class FearEffect extends MobEffect {
+    public static final int ARMOR_COLOR = ARGB.color(0x7a, 0x04, 0x21);
+
     public FearEffect(MobEffectCategory category, int color) {
         super(category, color, SParticleTypes.BAT);
     }
