6cf9370941979b4f9c5d3375c17c1c95f964d061
Ivan Filipenkov <fox@vanutp.dev>
2026-08-05 21:16:05 +0200

    fix heat visions. fixes #139
    - spawning heat vision no longer crashes the game
    - desert visions now despawn

--- src/main/java/com/github/thedeathlycow/scorchful/mixin/EntityMixin.java
+++ src/main/java/com/github/thedeathlycow/scorchful/mixin/EntityMixin.java
@@ -1,14 +1,20 @@
 package com.github.thedeathlycow.scorchful.mixin;
 
+import com.github.thedeathlycow.scorchful.attachment.EntityDesertVisionAttachment;
 import com.github.thedeathlycow.scorchful.attachment.ScorchfulEntityAttachments;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
-public class EntityMixin {
+public abstract class EntityMixin {
+    @Shadow
+    public abstract Level level();
 
     @Inject(
             method = "shouldBeSaved",
@@ -22,4 +28,18 @@ public class EntityMixin {
         }
     }
 
+    @Inject(
+            method = "tick",
+            at = @At("TAIL")
+    )
+    public void tickVision(CallbackInfo ci) {
+        if (this.level().isClientSide) {
+            return;
+        }
+        Entity instance = (Entity) (Object) this;
+        EntityDesertVisionAttachment attachment = instance.getData(ScorchfulEntityAttachments.ENTITY_DESERT_VISION);
+        if (attachment.hasDesertVision()) {
+            attachment.serverTick();
+        }
+    }
 }
